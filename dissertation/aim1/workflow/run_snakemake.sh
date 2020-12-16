85970144d5ebd2f4c523c5058d75081f3ed7178a
slsevilla <slsevilla@gmail.com>
2020-12-15 20:52:00 -0500

    update ref classifiers

--- dissertation/aim1/workflow/run_snakemake.sh
+++ dissertation/aim1/workflow/run_snakemake.sh
@@ -2,6 +2,7 @@ pipeline=$1
 snakefile=$2
 output_dir='/data/sevillas2/gmu/log'
 home_dir='/home/sevillas2/git/gmu/dissertation/aim1/workflow'
+sbatch_dir='/home/sevillas2/sbatch'
 
 log_time=`date +"%Y%m%d_%H%M"`
 
@@ -11,12 +12,12 @@ if [[ $pipeline = "cluster" ]] || [[ $pipeline = "local" ]]; then
   #if cluster - submit job
   if [[ $pipeline = "cluster" ]]; then
     #submit job to cluster
-    sbatch --job-name="gm" --gres=lscratch:200 --time=120:00:00 --mail-pipeline=BEGIN,END,FAIL \
+    sbatch --job-name="gmu" --gres=lscratch:200 --time=120:00:00 --output=${sbatch_dir}/%j_%x.out \
     snakemake --latency-wait 120  -s ${home_dir}/$snakefile \
     --printshellcmds --cluster-config ${home_dir}/cluster_config.yml --keep-going \
     --restart-times 1 --cluster "sbatch --gres {cluster.gres} --cpus-per-task {cluster.threads} \
-    -p {cluster.partition} -t {cluster.time} --mem {cluster.mem} --cores {cluster.cores} \
-    --job-name={params.rname} --output=${output_dir}/log/${log_time}_{params.rname}.out" -j 500 --rerun-incomplete
+    -p {cluster.partition} -t {cluster.time} --mem {cluster.mem} \
+    --job-name={params.rname} --output=${output_dir}/${log_time}_{params.rname}.out" -j 500 --rerun-incomplete
 
   #otherwise submit job locally
   else
