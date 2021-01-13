5c53a5250f1badec84aa480b5d6b0744ff3fcf70
slsevilla <slsevilla@gmail.com>
2021-01-13 14:26:31 -0500

    restructure to match other aims

--- dissertation/aim3/notebooks/01_create_slurm.sh
+++
@@ -1,5 +0,0 @@
-input_file="/home/sevillas2/git/gmu/dissertation/aim3/manifest/ftp_list.txt"
-slurm_file="/data/sevillas2/gmu/aim3/aim3_download.slurm"
-output_dir="/data/sevillas2/gmu/aim3/"
-
-awk -v out_dir="$output_dir" '{ print "wget -P " out_dir " " $1 }' $input_file > $slurm_file
--- dissertation/aim3/notebooks/02_submit_slurm.sh
+++
@@ -1,3 +0,0 @@
-slurm_dir="/data/sevillas2/gmu/aim3/aim3_download.slurm"
-
-swarm -f $slurm_dir --verbose 1
